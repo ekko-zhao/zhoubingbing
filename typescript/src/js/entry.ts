0d70416d75e05ee663745e69b74a78ec9d7d7122
zhoubbmls <zhoubbmls@sina.com>
2017-05-03 17:37:13 +0800

    5-3

--- typescript/src/js/entry.ts
+++ typescript/src/js/entry.ts
@@ -1,18 +1,65 @@
 
+//import * as c from'./hello.ts';
 
-import {default as Car} from'./hello.ts';
 
-class Dazhong extends Car{
-	drive(distanceInMeters: number=100){
-		console.log(`B car run ${distanceInMeters}m power by `+ this.engine)
+
+
+
+
+@Component({
+	seletor: 'person',
+	template: 'person.html'
+})
+class Person{
+	constructor(public firstName: string, public secondName: string){
+		
+	};
+	methods():void{
+		
 	}
 }
 
+function Component(component){
+	return (target:any) => {
+		return componentClass(target, component);
+	}
+}
 
-/*
-var Acar  = new Car('beichis');
-Acar.drive(100);
-*/
+function componentClass(target:any, component){
+	var origin = target;
+	//console.log(constructor);
+	function construct( constructor, args){
+		let c: any = function(){
+			return constructor.apply(this, args);
+		};
+		c.prototype = constructor.prototype;
+		return new c();
+	}
+	let f: any = (...args) => {
+		console.log('seletor: ' + component.seletor);
+		console.log('template: ' + component.template);
+		console.log(`person: ${origin.name}(${JSON.stringify(args)})`);
+		return construct(origin, args);
+		//return new target;
+	}
+	f.prototype = origin.prototype;
+	return f;
+}
 
-var Acar  = new Dazhong('Dazhong');
-Acar.drive(100);
+let p = new Person('angular','JS');
+
+console.log(p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- typescript/src/js/hello.ts
+++ typescript/src/js/hello.ts
@@ -1,14 +1,4 @@
 ﻿
 // 启用 --strictNullChecks
 
-class Car{
-	//engine: string;
-	constructor(public engine: string){
-		//this.engine = engine;
-	}
-	drive(distanceInMeters: number=0){
-		console.log(`A car run ${distanceInMeters}m power by `+ this.engine)
-	}
-}
 
-export default Car ;
