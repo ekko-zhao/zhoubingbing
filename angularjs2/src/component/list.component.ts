bbc2488708aa6ce5f0d9041c78e0c8e65850fadf
zhoubbmls <zhoubbmls@sina.com>
2017-05-11 17:56:06 +0800

    5-11

--- angularjs2/src/component/list.component.ts
+++ angularjs2/src/component/list.component.ts
@@ -4,22 +4,16 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 //import { ListComponent } from './component/item.component';
 //import { ListItemComponent } from './item.component';
 
-const EMAIL_REGEXP = new RegExp("[a-z0-9]+@[a-x0-9].com");
-const TEL_REGEXP = new RegExp("1[0-9]{10}");
-function validateFirstName(c: FormControl) {
-	return ( EMAIL_REGEXP.test(c.value) || TEL_REGEXP.test(c.value) ) ? null : {
-		firstName:{
-			valid:false,
-			errorMsg:'用户名必须是手机号或者邮箱地址'
-		}
-	}
-}
+
 
 @Component({
 	selector: 'list',
 	template: `
-		<p>{{sex | sexReform }}</p>
-		<p>{{json | json}}</p>
+		<form [formGroup]="editform">
+			<input type="text" formGroupName="firstname" [formControl]="firstname2">
+			<p>{{firstname2.value}}</p>
+			<button type="button" (click)="submit(editform)">button</button>
+		</form>
 	`,
 	
 	styles: [
@@ -28,15 +22,16 @@ function validateFirstName(c: FormControl) {
 	]
 })
 export class ListComponent {
-	name:number = 23.118
-	sex:string = 'male'
+	editform: FormGroup ;
 	constructor(){
-		setTimeout(()=>{
-			this.json.age=12
-		},2000)
+		this.editform = new FormGroup({
+			firstname: new FormControl('')
+		})
 	}
-	getData(){
-		return new Date();
+	firstname2: FormControl = new FormControl();
+	
+	
+	submit(c:object){
+		console.log(c)
 	}
-	json = {age:23, school:'xuexiao'}
 }
