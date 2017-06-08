e0596954a1e7401e4b725b530b643c1a5e8ade67
zhoubbmls <zhoubbmls@sina.com>
2017-06-08 18:15:29 +0800

    8

--- ES6/src/es5.js
+++ ES6/src/es5.js
@@ -1,3 +1,11 @@
 'use strict';
 
-var map = new Map([[1, 'one'], [2, 'tow'], [3, 'three']]);
+var _boj;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var boj = (_boj = {}, _defineProperty(_boj, Symbol('a'), 'hello'), _defineProperty(_boj, Symbol('b'), 'world'), _boj);
+
+var objectSymbol = Object.getOwnPropertySymbol(boj);
+
+console.log(objectSymbol);
--- ES6/src/es6.js
+++ ES6/src/es6.js
@@ -1,18 +1,3 @@
 
-
-let a = {
-	0:'a',
-	1:'b',
-	2:'c',
-	length:3,
-	[Symbol.iterator]: Array.prototype[Symbol.iterator]
-}
-let b = a[Symbol.iterator]()
-//console.log(...a[1])
-/*for (let i of a){
-	console.log(i)
-}*/
-console.log(b.next())
-console.log(b.next())
-console.log(b.next())
-console.log(b.next())
+var a = new Set([1,2,2,3]);
+console.log([...a])
