00763a2b6e6cd550b0ad828b2a2cdc83445a4ab0
zhoubbmls <zhoubbmls@sina.com>
2017-06-08 16:35:30 +0800

    8

--- ES6/src/es6.js
+++ ES6/src/es6.js
@@ -1,5 +1,18 @@
-let map = new Map([
-	[1,'one'],
-	[2,'tow'],
-	[3,'three']
-])
+
+
+let a = {
+	0:'a',
+	1:'b',
+	2:'c',
+	length:3,
+	[Symbol.iterator]: Array.prototype[Symbol.iterator]
+}
+let b = a[Symbol.iterator]()
+//console.log(...a[1])
+/*for (let i of a){
+	console.log(i)
+}*/
+console.log(b.next())
+console.log(b.next())
+console.log(b.next())
+console.log(b.next())
