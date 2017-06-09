284eda533cf007c7dcebe30815234dfbb959cfe8
zhoubbmls <zhoubbmls@sina.com>
2017-06-09 17:31:55 +0800

    1

--- ES6/src/es5.js
+++ ES6/src/es5.js
@@ -1,11 +1,3 @@
-'use strict';
+"use strict";
 
-var _boj;
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var boj = (_boj = {}, _defineProperty(_boj, Symbol('a'), 'hello'), _defineProperty(_boj, Symbol('b'), 'world'), _boj);
-
-var objectSymbol = Object.getOwnPropertySymbol(boj);
-
-console.log(objectSymbol);
+console.log(Promise);
--- ES6/src/es6.js
+++ ES6/src/es6.js
@@ -1,3 +1,1 @@
-
-var a = new Set([1,2,2,3]);
-console.log([...a])
+console.log(Promise)
