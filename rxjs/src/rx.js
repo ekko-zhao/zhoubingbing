bddaeeec694dff4f59616b1302c70dfd3373a6dd
zhoubbmls <zhoubbmls@sina.com>
2017-06-15 17:57:30 +0800

    15

--- rxjs/src/rx.js
+++ rxjs/src/rx.js
@@ -4,8 +4,7 @@ var Rx = require('rxjs/Rx');
 
 //var document = document || {}
 
-var button = document.querySelector('body');
-	Rx.Observable.fromEvent(button, 'click')
-	  .throttleTime(1000)
-	  .scan(count => count + 1, 0)
-	  .subscribe(count => console.log(count));
+Rx = Rx || Rx2
+
+
+console.log(Rx.Observable.prototype)
--- rxjs/src/rx5.js
+++ rxjs/src/rx5.js
@@ -2,9 +2,6 @@
 
 var Rx = require('rxjs/Rx');
 
-var button = document.querySelector('body');
-Rx.Observable.fromEvent(button, 'click').throttleTime(1000).scan(function (count) {
-	return count + 1;
-}, 0).subscribe(function (count) {
-	return console.log(count);
-});
+Rx = Rx || Rx2;
+
+console.log(Rx.Observable.prototype);
