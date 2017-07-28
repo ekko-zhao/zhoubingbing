5315e5758596283b7dd016c62f304f7e58fab873
zhoubbmls <zhoubbmls@sina.com>
2017-07-28 18:05:50 +0800

    2

--- ionic/src/pages/buttons/buttons.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-import { Platform, ActionSheetController } from 'ionic-angular';
-
-@Component({
-    templateUrl: 'buttons.component.html'
-})
-export class ButtonsComponent {
-
-}
