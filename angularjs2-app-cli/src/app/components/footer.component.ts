f9ebe25d81b9bda5921265fc8c6c52d9d8dceefc
zhoubbmls <zhoubbmls@sina.com>
2017-09-09 21:34:33 +0800

    9

--- angularjs2-app-cli/src/app/components/footer.component.ts
+++ angularjs2-app-cli/src/app/components/footer.component.ts
@@ -2,20 +2,37 @@ import { Component } from '@angular/core';
 //import { Promise } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 
+function aa(v: string) {
+    console.log(v)
+    return function aa(target, key) {
+        console.log(target)
+        console.log(key);
+        //return 'dddddddddddd'
+    }
+}
+
 
 @Component({
-	//selector: 'footer',
-	template: `<h3>footer</h3>`,
-	styles: [
-		`h3{border-top:1px #333 solid; color:red;}`,
-		`h3{ text-align:center}`
-	]
+    //selector: 'footer',
+    template: `<h3>footer:{{ name}}</h3>`,
+    styles: [
+        `h3{border-top:1px #333 solid; color:red;}`,
+        `h3{ text-align:center}`
+    ]
 })
-export class FooterComponent{
-	name:string = 'zhoubingbing'
-	constructor(){
+export class FooterComponent {
+    @aa('adsf')
+    public name: string;
+
+    public setname(name: string) {
+        this.name = name;
+    }
 
 
-	}
+
+    constructor() {
+        //var a = this.setname('zhoub1b');
+    }
+
 
 }
