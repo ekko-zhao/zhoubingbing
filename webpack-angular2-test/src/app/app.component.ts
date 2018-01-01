d6f443937734dda8d4842dda36ef156501941c95
zhoubbmls <zhoubbmls@sina.com>
2018-01-01 23:16:28 +0800

    home-1-1

--- webpack-angular2-test/src/app/app.component.ts
+++ webpack-angular2-test/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 
+var co = require('co');
+// var co = import('co');
+// console.log(co)
 @Component({
     selector: 'app-root',
     template: '<p>app-root</p>'
@@ -7,20 +10,35 @@ import { Component, ViewChild } from '@angular/core';
 
 export class AppComponent {
     constructor() {
-        function *abc(){
-            while(true){
-                yield 'a';
-                console.log('a')
-                yield 'b';
-                console.log('b')
-            }
+        var promise1 = new Promise(function (resolve, reject) {
+            resolve('resolve1');
+        })
 
+        var promise2 = new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                console.log(3)
+                resolve('resolve2');
+            }, 2000)
+        })
+
+        var f = function* () {
+            var a = yield 1;
+            console.log(a)
+            console.log(55)
+            yield promise2;
+            console.log(66)
+            return 'abc';
         }
-        var a= abc()
-        a.next()
-        a.next()
-        a.next()
-        a.next()
+
+
+        co(f).then(function (data) {
+            console.log(data)
+            console.log('resolve')
+        }, function (data) {
+            console.log(data)
+            console.log('reject')
+        })
+
 
         /* var a = objectentries(obj);
         console.log(a);
