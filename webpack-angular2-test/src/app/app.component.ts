0bb3fddf7a24747aea90196b4c5a8c5ea9bed30c
zhoubbmls <zhoubbmls@sina.com>
2018-01-08 22:56:16 +0800

    4

--- webpack-angular2-test/src/app/app.component.ts
+++ webpack-angular2-test/src/app/app.component.ts
@@ -1,16 +1,41 @@
 import { Component, ViewChild } from '@angular/core';
 
+var co = require('co');
+// var co = import('co');
+// console.log(co)
 @Component({
     selector: 'app-root',
     template: '<p>app-root</p>'
 })
 
 export class AppComponent {
-    public names: string = 'zhoubingbign';
-    public name() { };
     constructor() {
-        class MyArray extends Array { }
-        var a = new MyArray()
-        console.log(a)
+        function decorate(target, name, descriptor) {
+            var value = descriptor.value
+            descriptor.value = function(){
+                value.apply(target)
+                //console.log(this)
+            }
+        }
+
+
+        class foo {
+            age = 23
+            @decorate
+            getname() {
+                // console.log(this)
+                alert(1)
+                this.getfi()
+            }
+            getfi(){
+                console.log('getfi')
+            }
+            constructor(){
+            }
+        }
+
+        var a = new foo()
+        a.getname()
+        // a.getname = function(){}
     }
 }
