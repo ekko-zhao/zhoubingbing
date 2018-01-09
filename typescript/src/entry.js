55c492af00a60bfbc3dc4f47564c83af5a9609c4
zhoubbmls <zhoubbmls@sina.com>
2018-01-09 18:05:01 +0800

    9

--- typescript/src/entry.js
+++ typescript/src/entry.js
@@ -1,9 +1,15 @@
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = { firstName: "Jane", lastName: "User" };
+var user = {
+    firstName: "Jane",
+    lastName: "User"
+};
 document.body.innerHTML = greeter(user);
-var _a = [2, 3, 5], a = _a[0], b = _a[1], c = _a[2];
+var _a = [2, 3, 5],
+    a = _a[0],
+    b = _a[1],
+    c = _a[2];
 var abc = [5, 6, 7];
 for (var _i = 0, abc_1 = abc; _i < abc_1.length; _i++) {
     var item = abc_1[_i];
