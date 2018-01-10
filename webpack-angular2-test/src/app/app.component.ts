c62a20d2b9fdcfe110d92f1bab949dd913bd9a36
zhoubbmls <zhoubbmls@sina.com>
2018-01-10 18:09:00 +0800

    10

--- webpack-angular2-test/src/app/app.component.ts
+++ webpack-angular2-test/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 
-var co = require('co');
-// var co = import('co');
-// console.log(co)
 @Component({
     selector: 'app-root',
     template: '<p>app-root</p>'
@@ -10,32 +7,6 @@ var co = require('co');
 
 export class AppComponent {
     constructor() {
-        function decorate(target, name, descriptor) {
-            var value = descriptor.value
-            descriptor.value = function(){
-                value.apply(target)
-                //console.log(this)
-            }
-        }
 
-
-        class foo {
-            age = 23
-            @decorate
-            getname() {
-                // console.log(this)
-                alert(1)
-                this.getfi()
-            }
-            getfi(){
-                console.log('getfi')
-            }
-            constructor(){
-            }
-        }
-
-        var a = new foo()
-        a.getname()
-        // a.getname = function(){}
     }
 }
--- webpack-angular2-test/webpack.config.js
+++ webpack-angular2-test/webpack.config.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');
 const path = require('path');
-
+const {
+    TsConfigPathsPlugin
+} = require('awesome-typescript-loader');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -41,31 +43,36 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.tsx?$/,
-            use: [
-                // 'babel-loader',
-                'awesome-typescript-loader'
-            ],
-            exclude: /node_modules/
-        }, {
-            test: /\.(gif|svg|woff|woff2|eot|ttf|otf)$/,
-            use: [
-                'file-loader?name=[path][name].[ext]'
-            ]
-        }, {
-            test: /\.(png|jpg)$/,
-            use: 'url-loader?limit=8192'
-        }/* , {
-            test: /\.html?$/,
-            use: [
-                'html-loader'
-            ]
-        } */]
-    },
-    node: {
-        fs: 'empty'
+                test: /\.tsx?$/,
+                use: [
+                    // 'babel-loader',
+                    'awesome-typescript-loader'
+                ],
+                exclude: /node_modules/
+            }, {
+                test: /\.(gif|svg|woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader?name=[path][name].[ext]'
+                ]
+            }, {
+                test: /\.(png|jpg)$/,
+                use: 'url-loader?limit=8192'
+            }
+            /* , {
+                        test: /\.html?$/,
+                        use: [
+                            'html-loader'
+                        ]
+                    } */
+        ]
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.ts', '.tsx', '.js'],
+        plugins: [
+            new TsConfigPathsPlugin({
+                configFileName: "tsconfig.json",
+                compiler: "typescript",
+            })
+        ]
     }
 }
