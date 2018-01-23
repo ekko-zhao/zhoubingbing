dac8af9b2db05880ffe1bc3c8c136257dc30b2ef
zhoubbmls <zhoubbmls@sina.com>
2018-01-23 18:12:26 +0800

    22

--- webpack-angular2-test/src/app/app.component.ts
+++ webpack-angular2-test/src/app/app.component.ts
@@ -1,6 +1,6 @@
 
 import { Component } from '@angular/core';
-import * as Rx from 'rxjs';
+/* import * as Rx from 'rxjs'; */
 
 @Component({
     selector: 'app-root',
@@ -11,18 +11,6 @@ import * as Rx from 'rxjs';
 })
 export class AppComponent {
     constructor() {
-        var subject = new Rx.ReplaySubject(3); // buffer 3 values for ne w subscribers ，注:缓存了三个值。
-        subject.subscribe({ next: (v) => console.log('observerA: ' + v) });
-        subject.next(1);
-        subject.next(2);
-        subject.next(3);
-        subject.next(4);
-
-        subject.subscribe({
-            next: (v) => console.log('observerB: ' + v)
-        });
-
-        subject.next(5);
 
     }
 }
