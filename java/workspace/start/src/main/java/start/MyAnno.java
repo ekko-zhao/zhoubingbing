e534f8895a73eee6e083328a8a6dc9ef65a4f1cc
zhoubbmls <zhoubbmls@sina.com>
2018-02-05 18:49:16 +0800

    2-2

--- java/workspace/start/src/main/java/start/MyAnno.java
+++ java/workspace/start/src/main/java/start/MyAnno.java
@@ -1,6 +1,10 @@
 package start;
 
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnno {
 	String str();
+
 	int val();
 }
--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -1,9 +1,27 @@
 package start;
 
+import java.lang.reflect.*;
+
 public class Test {
+	@MyAnno(str = "Annotation Example", val = 99)
+	public static void myMeth2() {
+	}
+	
+	@MyAnno(str = "Annotation Example", val = 100)
+	public static void myMeth() {
+		Test t = new Test();
+		
+		try {
+			Class<?> c = t.getClass();
+			Method m = c.getMethod("myMeth2");
+			MyAnno anno = m.getAnnotation(MyAnno.class);
+			System.out.println(anno.str() + " " + anno.val());
+		} catch (NoSuchMethodException e) {
+			System.out.println("Method not find");
+		}
+	}
 
 	public static void main(String[] args) {
-		T1 t1 = new T1();
-		System.out.println();
+		myMeth();
 	}
 }
