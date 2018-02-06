7bdb5a7acbc864a93afc55b8f8222c863d50cf6e
zhoubbmls <zhoubbmls@sina.com>
2018-02-06 18:06:40 +0800

    2-6

--- java/workspace/start/src/main/java/start/MyAnno.java
+++ java/workspace/start/src/main/java/start/MyAnno.java
@@ -3,8 +3,14 @@ package start;
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(MyRepeatedAnnos.class)
 public @interface MyAnno {
 	String str();
 
 	int val();
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyRepeatedAnnos {
+	MyAnno[] value();
 }
--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -1,27 +1,27 @@
 package start;
 
-import java.lang.reflect.*;
+import java.io.*;
 
 public class Test {
-	@MyAnno(str = "Annotation Example", val = 99)
-	public static void myMeth2() {
-	}
-	
-	@MyAnno(str = "Annotation Example", val = 100)
-	public static void myMeth() {
-		Test t = new Test();
-		
-		try {
-			Class<?> c = t.getClass();
-			Method m = c.getMethod("myMeth2");
-			MyAnno anno = m.getAnnotation(MyAnno.class);
-			System.out.println(anno.str() + " " + anno.val());
-		} catch (NoSuchMethodException e) {
-			System.out.println("Method not find");
+	public static void main(String[] args) throws IOException {
+		// System.out.println();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str[] = new String[100];
+		for (int i = 0; i < 100; i++) {
+			str[i] = br.readLine();
+			if (str[i].equals("stop"))
+				break;
 		}
-	}
+		for (int i = 0; i < 100; i++) {
+			if (str[i].equals("stop"))
+				break;
+			System.out.println(str[i]);
+		}
+		System.out.println("22");
+		/*
+		 * String str; do { str = br.readLine(); System.out.println(str); }
+		 * while (!str.equals("stop"));
+		 */
 
-	public static void main(String[] args) {
-		myMeth();
 	}
 }
