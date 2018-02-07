9912ead93f2d6ad17f914cb7a718da84df60aad5
zhoubbmls <zhoubbmls@sina.com>
2018-02-07 18:07:27 +0800

    2-7

--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -1,27 +1,13 @@
 package start;
 
-import java.io.*;
+public class Test<T> {
+	void showType(String str) {
+		System.out.println(str.getClass().getName());
+	}
 
-public class Test {
-	public static void main(String[] args) throws IOException {
-		// System.out.println();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str[] = new String[100];
-		for (int i = 0; i < 100; i++) {
-			str[i] = br.readLine();
-			if (str[i].equals("stop"))
-				break;
-		}
-		for (int i = 0; i < 100; i++) {
-			if (str[i].equals("stop"))
-				break;
-			System.out.println(str[i]);
-		}
-		System.out.println("22");
-		/*
-		 * String str; do { str = br.readLine(); System.out.println(str); }
-		 * while (!str.equals("stop"));
-		 */
-
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.showType("2323");
+		// System.out.println("22");
 	}
 }
