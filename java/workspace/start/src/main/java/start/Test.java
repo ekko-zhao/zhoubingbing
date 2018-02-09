9b2cbf294c3a9381a70745b1a510ed79e67ff0be
zhoubbmls <zhoubbmls@sina.com>
2018-02-09 15:53:00 +0800

    2-9

--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -1,17 +1,27 @@
 package start;
 
-public class Test<T> {
-	static double num = 10.00;
+interface MyFunc<O, T> {
+	O[] func(T v);
+}
 
+class MyClass<T> {
+	public T val;
+
+	MyClass(T v) {
+		val = v;
+	}
+
+	T getVal() {
+		return val;
+	}
+}
+
+public class Test<T> {
 	public static void main(String[] args) {
-		int i = 10;
+		MyFunc<MyClass<Integer>, Integer> f = MyClass[]::new;
 		
-		T1 m;
-		m = () -> {
-			Test.num = 1;
-			return Test.num;
-		};
-		
-		System.out.println(m.m());
+		MyClass[] myClass = f.func(10);
+		myClass[0] = new MyClass(10);
+		myClass[1] = new MyClass(20);
 	}
 }
