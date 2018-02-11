da442dce72e04e45405d42b08c9592ffa6814000
zhoubbmls <zhoubbmls@sina.com>
2018-02-11 18:05:32 +0800

    2-11

--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -1,27 +1,9 @@
 package start;
 
-interface MyFunc<O, T> {
-	O[] func(T v);
-}
-
-class MyClass<T> {
-	public T val;
-
-	MyClass(T v) {
-		val = v;
-	}
-
-	T getVal() {
-		return val;
-	}
-}
-
 public class Test<T> {
 	public static void main(String[] args) {
-		MyFunc<MyClass<Integer>, Integer> f = MyClass[]::new;
-		
-		MyClass[] myClass = f.func(10);
-		myClass[0] = new MyClass(10);
-		myClass[1] = new MyClass(20);
+		StringBuffer s = new StringBuffer("abc");
+		s.setCharAt(2, 'e');
+		System.out.println(s);
 	}
 }
