7c6cc574dd40ec6618dc0aa67ac80c10fcf84073
zhoubbmls <zhoubbmls@sina.com>
2018-02-12 14:33:47 +0800

    2-12

--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -2,8 +2,7 @@ package start;
 
 public class Test<T> {
 	public static void main(String[] args) {
-		StringBuffer s = new StringBuffer("abc");
-		s.setCharAt(2, 'e');
-		System.out.println(s);
+		
+		// System.out.println(a);
 	}
 }
