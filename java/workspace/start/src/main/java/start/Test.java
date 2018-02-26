2981adb81f9e5808c1a0ae4f19ece3e1cb62b366
zhoubbmls <zhoubbmls@sina.com>
2018-02-26 18:08:56 +0800

    2-26

--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -1,8 +1,10 @@
 package start;
 
+import java.util.*;
+
 public class Test<T> {
 	public static void main(String[] args) {
-		
+		Collection c = new ArrayList();
 		// System.out.println(a);
 	}
 }
