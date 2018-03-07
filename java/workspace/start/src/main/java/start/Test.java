01d2556585b7e1f8b59cea5b08607f4d114e8bf5
zhoubbmls <zhoubbmls@sina.com>
2018-03-07 18:01:42 +0800

    3-7

--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -4,7 +4,10 @@ import java.util.*;
 
 public class Test<T> {
 	public static void main(String[] args) {
+		Random r = new Random();
 		
-		System.out.println(Calendar.getInstance().getTime());
+		// TimeZone tz = new TimeZone();
+		System.out.println(r.nextInt(100));
+		
 	}
 }
