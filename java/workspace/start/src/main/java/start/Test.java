1b813c84797fb6d5aeffdc1c6424508ef54f663a
zhoubbmls <zhoubbmls@sina.com>
2018-03-09 17:51:05 +0800

    3-9

--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -1,16 +1,22 @@
 package start;
 
-
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class Test<T> {
-	public static void main(String[] args) throws ParseException, InterruptedException {
-		Formatter f = new Formatter();
-		f.format("%tT", new Date());
+public class Test {
+	public static void main(String[] args) throws IOException {
+		String instr = "Name: Tom Age: 28 id:77";
+		Scanner conin = new Scanner(instr);
+		conin.skip("Named");
+			// s -> "Age:"
+		if(conin.hasNext())
+			System.out.println(conin.next());
+			// 28
+		conin.close();
 		
-		System.out.println(f);
-		
+		// System.out.println();
 	}
 }
