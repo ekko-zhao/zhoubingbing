a667c096d8bef92354f7e7f4bbf9a734aacb14db
zhoubbmls <zhoubbmls@sina.com>
2018-03-12 18:02:07 +0800

    3-12

--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -1,22 +1,43 @@
 package start;
 
-import java.io.FileWriter;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Test {
 	public static void main(String[] args) throws IOException {
-		String instr = "Name: Tom Age: 28 id:77";
-		Scanner conin = new Scanner(instr);
-		conin.skip("Named");
-			// s -> "Age:"
-		if(conin.hasNext())
-			System.out.println(conin.next());
-			// 28
-		conin.close();
+		File file = new File("file/abc.txt");
+		FileInputStream fi = new FileInputStream(file);
 		
+		BufferedInputStream f = new BufferedInputStream(fi);
+		
+		f.mark(1);
+		System.out.println(f.read());
+		f.reset();
+		System.out.println(f.read());
+		System.out.println(f.read());
+		
+		/*System.out.println(f.read());
+		System.out.println(f.read());
+		
+		System.out.println(f.read());
+		System.out.println(f.read());
+		System.out.println(f.read());
+		System.out.println(f.read());
+		System.out.println(f.read());
+		f.reset();
+		System.out.println(f.read());
+		System.out.println(f.read());
+		f.reset();
+		System.out.println(f.read());
+		System.out.println(f.read());
+		System.out.println(f.read());*/
+		
+		fi.close();
+
 		// System.out.println();
 	}
 }
