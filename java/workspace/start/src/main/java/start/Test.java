461ab142e6b30b7d267d903922b7c5b99ed92759
zhoubbmls <zhoubbmls@sina.com>
2018-03-14 18:08:31 +0800

    3-14

--- java/workspace/start/src/main/java/start/Test.java
+++ java/workspace/start/src/main/java/start/Test.java
@@ -1,27 +1,23 @@
 package start;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PushbackInputStream;
+import java.io.*;
 import java.util.*;
 
+class Student implements Serializable {
+	private String name;
+
+	public Student(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
+
 public class Test {
-	public static void main(String[] args) throws IOException {
-		try (FileReader fr = new FileReader("file/abc.txt")) {
-			System.out.println((char)fr.read());
-		} catch (IOException e) {
-			
-		}
-		char c = 'a';
-		 System.out.println(c);
+	public static void main(String[] args) throws Exception {
+		
 		// System.out.println();
 	}
 }
