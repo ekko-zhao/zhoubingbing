c9f4e2dd5515270052c9ad605940e12d085ca9df
zhoubbmls <zhoubbmls@sina.com>
2018-03-26 22:51:16 +0800

    326

--- java/workspace/test/src/main/java/com/smart/TestDome.java
+++ java/workspace/test/src/main/java/com/smart/TestDome.java
@@ -3,24 +3,14 @@ package com.smart;
 import org.junit.Test;
 import org.springframework.aop.BeforeAdvice;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestDome {
 	@Test
 	public void befor(){
-		NaiveWaiter target = new NaiveWaiter();
-		BeforeAdvice advice = new GreetingBeforeAdvice();
-		
-		// Spring 提供的代理工厂
-		ProxyFactory pf = new ProxyFactory();
-		
-		// 设置代理目标
-		pf.setTarget(target);
-		
-		// 为代理目标添加增强
-		pf.addAdvice(advice);
-		
-		// 生成代理实例
-		NaiveWaiter proxy = (NaiveWaiter)pf.getProxy();
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/smart/beanfactory/beans.xml");
+		NaiveWaiter proxy = (NaiveWaiter) ctx.getBean("waiter");
 		proxy.greetTo();
 	}
 }
