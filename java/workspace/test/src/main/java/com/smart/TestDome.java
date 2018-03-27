82a64103b53b400d266e89e9c3de5aa1d5fdfc70
zhoubbmls <zhoubbmls@sina.com>
2018-03-27 18:44:42 +0800

    3-27

--- java/workspace/test/src/main/java/com/smart/TestDome.java
+++ java/workspace/test/src/main/java/com/smart/TestDome.java
@@ -2,6 +2,7 @@ package com.smart;
 
 import org.junit.Test;
 import org.springframework.aop.BeforeAdvice;
+import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,10 +10,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestDome {
 	@Test
 	public void befor(){
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/smart/beanfactory/beans.xml");
-		Waiter proxy = (Waiter) ctx.getBean("waiter");
-		proxy.greetTo();
-		proxy.serveTo();
-		
+		/*ApplicationContext ctx = new ClassPathXmlApplicationContext("com/smart/beanfactory/beans.xml");
+		Waiter waiter = (Waiter)ctx.getBean("waiter");
+		waiter.greetTo();*/
 	}
 }
