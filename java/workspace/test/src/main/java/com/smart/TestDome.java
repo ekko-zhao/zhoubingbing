bb6e01f764593ef80f1a2389c2ae60647503a131
zhoubbmls <zhoubbmls@sina.com>
2018-04-03 18:06:04 +0800

    4-3

--- java/workspace/test/src/main/java/com/smart/TestDome.java
+++ java/workspace/test/src/main/java/com/smart/TestDome.java
@@ -1,24 +1,9 @@
 package com.smart;
 
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import org.junit.Test;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-
-/*class TestClient extends Thread{
-	
-}*/
-
 public class TestDome extends Thread {
 	
-	public void run(){
+	public static void main(String[] str)  {
 		
 	}
-	
-	public static void main(String[] str) throws InterruptedException {
-		
-	}
-
 	// System.out.println();
 }
