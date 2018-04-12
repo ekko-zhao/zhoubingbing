0d44085b95c351723bd24a4c89f42cabcc016a42
zhoubbmls <zhoubbmls@sina.com>
2018-04-12 15:49:16 +0800

    4-12

--- java/workspace/test/src/main/java/com/smart/job/JDBCJobStoreRunner.java
+++ java/workspace/test/src/main/java/com/smart/job/JDBCJobStoreRunner.java
@@ -11,22 +11,6 @@ import org.quartz.impl.StdSchedulerFactory;
 
 public class JDBCJobStoreRunner {
 	public static void main(String agrs[]) {
-		try {
-			SchedulerFactory schedulerFactory = new StdSchedulerFactory();
-			Scheduler scheduler = schedulerFactory.getScheduler();
-			String[] triggerGroups = scheduler.getTriggerGroupNames();
-			
-			for(int i=0; i < triggerGroups.length; i++) {
-				String[] triggers = scheduler.getTriggerNames(triggerGroups[i]);
-				for(int j = 0; j< triggers.length; j++) {
-					Trigger tg = scheduler.getTrigger(triggers[j], triggerGroups[i]);
-					
-					if(tg instanceof SimpleTrigger && tg.getFullName().equals("tgroup1.grigger1_1")) {
-						scheduler.rescheduleJob(triggers[j], triggerGroups[i], tg);
-					}
-				}
-			}
-			scheduler.start();
-		}catch(Exception e) {}
+		
 	}
 }
