b5fa5d73b7818e474ee22dcad202de4ca8635460
zhoubbmls <zhoubbmls@sina.com>
2018-04-13 08:24:48 +0800

    413

--- java/workspace/test/src/main/java/com/smart/controller/UserController.java
+++ java/workspace/test/src/main/java/com/smart/controller/UserController.java
@@ -2,16 +2,18 @@ package com.smart.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/")
 public class UserController {
-	@RequestMapping("/register")
+	/*
+	 * @RequestMapping("/index") public @ResponseBody String test() { return
+	 * "hello, world! This com from spring!"; }
+	 */
+
+	@RequestMapping(value = { "/hello", "/index", "/", "" })
 	public String index() {
 		return "index";
 	}
-	@RequestMapping("/register")
-	public String register() {
-		return "index";
-	}
+
 }
