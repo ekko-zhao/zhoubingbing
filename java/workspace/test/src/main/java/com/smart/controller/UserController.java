56aa78d16554498397e2b9b0b8814a83d1e17f67
zhoubbmls <zhoubbmls@sina.com>
2018-04-13 17:59:20 +0800

    4-13

--- java/workspace/test/src/main/java/com/smart/controller/UserController.java
+++ java/workspace/test/src/main/java/com/smart/controller/UserController.java
@@ -1,19 +1,52 @@
 package com.smart.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.smart.entity.User;
+
 @Controller
+@RequestMapping(value = {"/"})
 public class UserController {
 	/*
 	 * @RequestMapping("/index") public @ResponseBody String test() { return
 	 * "hello, world! This com from spring!"; }
 	 */
-
-	@RequestMapping(value = { "/hello", "/index", "/", "" })
-	public String index() {
+	// @CookieValue("JSESSIONID") String sessionId
+	@RequestMapping(value = {"/index", ""})
+	public String index(HttpServletRequest request) {
+		System.out.println(request.getSession().getId());
 		return "index";
 	}
-
+	
+	
+	@RequestMapping(value = {"/success"})
+	public String success() {
+		return "success";
+	}
+	
+	@RequestMapping(value = {"/register"}, method=RequestMethod.POST)
+	public String register(User user) {
+		// System.out.println(sessionId);
+		
+		System.out.println("register");
+		System.out.println(user.getUserName());
+		return "success";
+	}
+	
 }
+
+
+
+
+
+
+
+
