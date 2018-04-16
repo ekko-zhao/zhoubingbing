61f99627cd72704da0160e12b8f8ddf0ce327a56
zhoubbmls <zhoubbmls@sina.com>
2018-04-16 18:06:07 +0800

    4-16

--- java/workspace/test/src/main/java/com/smart/controller/UserController.java
+++ java/workspace/test/src/main/java/com/smart/controller/UserController.java
@@ -1,19 +1,31 @@
 package com.smart.controller;
 
+import java.io.IOException;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.smart.entity.User;
 
@@ -21,8 +33,10 @@ import com.smart.entity.User;
 @RequestMapping("/")
 public class UserController {
 	/*
-	 * @RequestMapping("/index") public @ResponseBody String test() { return
-	 * "hello, world! This com from spring!"; }
+	 * @RequestMapping("/index") 
+	 * public @ResponseBody String test() { 
+	 * return "hello, world! This com from spring!"; 
+	 * }
 	 */
 
 	@RequestMapping(path = { "index", "" })
@@ -37,13 +51,28 @@ public class UserController {
 	public String success() {
 		return "success";
 	}
-
-	@RequestMapping(path = { "api/register/{id}" }, method = RequestMethod.GET)
-	public String register(User user, @MatrixVariable(required=false) String author2, @MatrixVariable(value="author") Map<String,String> a) {
+	
+	
+	@RequestMapping(path = { "api/register" }, method = RequestMethod.POST)
+	public String register(User user, HttpSession httpSession) {
+		Enumeration name = httpSession.getAttributeNames();
+		if(name.hasMoreElements()) {
+			name.nextElement();
+		}
+		
 		System.out.println("register");
-		System.out.println(a.get("age"));
-		System.out.println(author2);
 		return "success";
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
