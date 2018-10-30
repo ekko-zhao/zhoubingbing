1c7c08664ad0cbd6ba32cdc1d821b5deeba6b131
zhoubbmls <zhoubbmls@sina.com>
2018-10-30 20:53:07 +0800

    10-30

--- java/workspace/test/src/main/java/com/smart/run.java
+++ java/workspace/test/src/main/java/com/smart/run.java
@@ -1,13 +1,30 @@
 package com.smart;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class run {
 	public static void main(String[] args) {
+		int i;
 		
-		int a = 2;
-		a++;
-		
-		System.out.println(a);
-		
-		
+		try (
+				FileInputStream fina = new FileInputStream("C:\\Users\\Administrator\\Desktop\\a.txt")) {
+			do {
+				i = fina.read();
+				if (i != -1)
+				{
+					System.out.println((char) i);
+					
+				}
+			} while (i != -1);
+		} catch (FileNotFoundException e) {
+		} catch (IOException e3) {}
+
 	}
 }
